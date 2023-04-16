dd8b1854b851ccf06cd4ace1a7d432c62525f61f
seykolla <shreyakolla@gmail.com>
2023-04-15 22:28:44 -0700

    added RENAME_ME.py

---
+++ code/lib/RENAME_ME.py
@@ -0,0 +1,11 @@
+secrets = {
+    "data": "data",
+    "status": "status",
+    "remote": "remote",
+    "homeSSID": "",
+    "homePass": "",
+    "broker": "",
+    "port": "",
+    "broker_username": "",
+    "broker_password": ""
+}
